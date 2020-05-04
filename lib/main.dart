52aeb00e546f2ecca95564d7a7919839fcb9f1aa
sifat hassan <sifat27.sh22@gmail.com>
2020-05-04 13:55:07 +0600

    input flied explore and retrive data

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,85 @@ void main() {
   runApp(myapp());
 }
 
-class myapp extends StatelessWidget {
+class myapp extends StatefulWidget {
+  @override
+  _myappState createState() => _myappState();
+}
+
+class _myappState extends State<myapp> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.yellow
-      ),
-      child: Center(
-        child: Image.asset('assets/images/xxx.png',
-        fit: BoxFit.contain,
-          height: 100.0,
-          width: 100.0,
-        ),
+    return  MaterialApp(
+      title: 'Title',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String value="";
+  String textd="";
+ void showtext()
+ {
+   setState(() {
+     textd=value;
+   });
+ }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "$textd",
+          ),
+          Padding(
+            padding: EdgeInsets.all(4.0),
+            child:
+              TextField(
+                onChanged: (text){
+                   value=text;
+                },
+                maxLength:20,
+                maxLines: 2,
+                decoration: InputDecoration(
+//                hintText:"user name",
+                    labelText: "user name",
+                    helperText: "plz enter name",
+                    border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.access_alarm),
+                ),
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 30.0,
+                  fontWeight:FontWeight.bold,
+
+                ),
+              ),
+          ),
+          MaterialButton(
+            onPressed:showtext,
+            child: Text("click me"),
+
+
+          )
+
+        ],
       ),
     );
   }
 }
+
 
 
 
