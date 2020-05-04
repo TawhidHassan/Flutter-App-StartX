256d505ce0097d53db38a3962052a5d70d53a849
sifat hassan <sifat27.sh22@gmail.com>
2020-05-04 13:25:58 +0600

    iamge use

--- lib/main.dart
+++ lib/main.dart
@@ -6,74 +6,24 @@ void main() {
   runApp(myapp());
 }
 
-class myapp extends StatefulWidget {
-  @override
-  _myappState createState() => _myappState();
-}
-
-class _myappState extends State<myapp> {
+class myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.yellow
       ),
-      title: "Statefull",
-      home: homepage(),
-    );
-  }
-}
-
-class homepage extends StatefulWidget {
-  @override
-  _homepageState createState() => _homepageState();
-}
-
-class _homepageState extends State<homepage> {
-  int dataChangevar=0;
-
-  void dataChange()
-  {
-    setState(() {
-      dataChangevar+=1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("AppBar",
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Text("$dataChangevar",
-              style:TextStyle(
-                color: Colors.red,
-                fontSize: 50.0,
-                backgroundColor: Colors.green
-              ),
-              ),
-            ),
-            MaterialButton(
-                onPressed: dataChange,
-              child: Text("click me",
-              style: TextStyle(
-                fontSize: 20.0,
-              ),
-              ),
-            )
-          ],
+      child: Center(
+        child: Image.asset('assets/images/xxx.png',
+        fit: BoxFit.contain,
+          height: 100.0,
+          width: 100.0,
         ),
       ),
     );
   }
 }
+
 
 
 
